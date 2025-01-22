40c371f2daf0d385c1da9acdf5bfa8b109c6d6f5
Shiyi Peng <shiyipeng@microsoft.com>
2025-01-22 13:56:43 -0800

    fix 10 ??

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -166,7 +166,7 @@ do_install_aduc_packages() {
     if [[ $OS == "debian" ]]; then
         $SUDO apt-get install --yes gcc-10 g++-10 || return
     else
-        $SUDO apt-get install --yes gcc-8 g++-8 || return
+        $SUDO apt-get install --yes gcc-10 g++-10 || return
     fi
 
     echo "Installing packages required for static analysis..."
