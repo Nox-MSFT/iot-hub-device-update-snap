d6351ba1855c3202acceb4f824d75599490b2cb3
Shiyi Peng <shiyipeng@microsoft.com>
2025-01-22 12:47:26 -0800

    gcc10

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -161,11 +161,10 @@ do_install_aduc_packages() {
 
     $SUDO apt-get install --yes "${aduc_packages[@]}" || return
 
-    # The latest version of gcc available on Debian is gcc-6. We install that version if we are
-    # building for Debian, otherwise we install gcc-8 for Ubuntu.
+    # Install gcc-10 for Debian, gcc-8 for Ubuntu
     OS=$(lsb_release --short --id)
-    if [[ $OS == "debian" && $VER == "9" ]]; then
-        $SUDO apt-get install --yes gcc-6 g++-6 || return
+    if [[ $OS == "debian" ]]; then
+        $SUDO apt-get install --yes gcc-10 g++-10 || return
     else
         $SUDO apt-get install --yes gcc-8 g++-8 || return
     fi
